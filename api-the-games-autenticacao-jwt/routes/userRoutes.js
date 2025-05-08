3a0607eea33a11f04cb50b01c0dbb2e1e595126b
rafdavis <rd506269@gmail.com>
2025-05-08 20:47:11 -0300

    docs: "Alteração no formato do código"

--- api-the-games-autenticacao-jwt/routes/userRoutes.js
+++ api-the-games-autenticacao-jwt/routes/userRoutes.js
@@ -2,10 +2,8 @@ import express from "express";
 const userRoutes = express.Router();
 import userController from "../controllers/userController.js";
 
-// Endpoint para cadastrar um usuário
 userRoutes.post("/user", userController.createUser);
 
-// Endpoint para autentificação do usuário
 userRoutes.post("/auth", userController.loginUser);
 
 export default userRoutes;
